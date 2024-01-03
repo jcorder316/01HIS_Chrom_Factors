684c106da99f3f600c1ccc26a34db8251959d268
jcorder316 <48988005+jcorder316@users.noreply.github.com>
2024-01-04 00:51:03 +0100

    Scripts for TFinZONE at 01dcpi
    Scripts for TFinZONE at 01dcpi

---
+++ 004Fig3_to_5_TFinZONE/01Fig3_MO_TFinZONE_01dpci/02Scr/01find_MO_on_Chr_State.sh
@@ -0,0 +1,42 @@
+#conda activate my_env
+
+SI=500
+SI2=200
+CO=16
+RA=30000
+LE=8
+NAME="CORR1_01d"
+mkdir MO_PERAME
+############## group 1   #############
+PP=..
+for (( i = 0; i <= 4; i++ ))
+ do
+for R1 in $PP/01DATA_TEST/04d*/0${i}*_04dcpi_forMOTIF.bed
+do
+yolo=$(echo "$R1" | rev | cut -c 5- | rev)
+yolob=$(echo "$R1" | rev | cut -c 13- | rev)
+yolob1=$(echo "$R1" | rev | cut -c 14- | rev)
+yolo1=$(echo "$yolo" | cut -c 30- )
+yolo2=$(echo "$yolob" |  cut -c 30- )
+for MM in $PP/01DATA_TEST/04d*/0${i}*_mask_500/knownResults/*.txt
+do
+mkdir ${R1%/*}/08MO_on_PEAKS
+yolo3=$(echo "$MM" | rev | cut -c 5- | rev)
+yolo4=$(echo "$yolo3" | cut -c 89- )
+
+echo "$MM2"
+echo "findMotifsGenome.pl $R1 danRer11 ${R1%/*}/08MO_on_PEAKS/`basename $yolob1`mask_500_FIND -size $SI -len 8 -h -mask  -p $CO -cache $RA -find $MM "
+echo "findMotifsGenome.pl $R1 danRer11 ${R1%/*}/08MO_on_PEAKS/`basename $yolob1`mask_500_FIND -size $SI -len 8 -h -mask  -p $CO -cache $RA -find $MM > ${R1%/*}/08MO_on_PEAKS/`basename $yolob1`mask_500_FIND/`basename $yolob``basename $yolo3`.txt"
+
+findMotifsGenome.pl $R1 danRer11 ${R1%/*}/08MO_on_PEAKS/`basename $yolob1`_mask_500_FIND -size $SI -len 8 -h -mask  -p $CO -cache $RA -find $MM 
+findMotifsGenome.pl $R1 danRer11 ${R1%/*}/08MO_on_PEAKS/`basename $yolob1`_mask_500_FIND -size $SI -len 8 -h -mask  -p $CO -cache $RA -find $MM > ${R1%/*}/08MO_on_PEAKS/`basename $yolob1`_mask_500_FIND/`basename $yolob``basename $yolo3`.txt
+mv ${R1%/*}/08MO_on_PEAKS/`basename $yolob1`_mask_500_FIND/motifFindingParameters.txt ./MO_PERAME/`basename $yolob1`_motifFindingParameters.txt
+
+find ${R1%/*}/08MO_on_PEAKS/`basename $yolob1`_mask_500_FIND/ -type f -name "*.txt" -size -400c -delete
+
+done 
+done
+
+done 
+
+
